f77d79b429c0b2ffbe2249de4b29e5e0c64067be
bogdano <bogdan.boskovic@outlook.com>
2024-02-16 09:50:30 -0700

    updated homepage

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>this isn't even my final form</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://zombo.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Zombo.com
         </a>
       </header>
     </div>
